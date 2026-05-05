d06fb25747108a860f1540bb3b2606e51309204c
Hamza <hamza@Hamzas-iMacPro.local>
2026-05-05 14:15:10 +0500

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
+import path from "node:path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  turbopack: {
+    root: path.join(__dirname),
+  },
 };
 
 export default nextConfig;
